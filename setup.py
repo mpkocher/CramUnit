9f0932b278033c94def0893963fd61e1047d76fc
mpkocher <michael.kocher@me.com>
2013-03-03 23:32:41 -0800

    Updated setup.py to be more compatible with pypi.j

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ except ImportError:
 
 version = __import__('cram_unit').get_version()
 
+with open('README.md') as f:
+    _long_description = f.read()
+
 setup(
     name='CramUnit',
     version=version,
@@ -12,7 +15,12 @@ setup(
     license='BSD',
     author='mpkocher',
     author_email='mkocher@pacificbiosciences.com',
+    url="http://github.com/mpkocher/CramUnit",
     description='Tool to create Unittests/Xunit output from cram (*.t) tests.',
     scripts=['bin/run_cram_unit.py'],
-    install_requires=['cram', 'nose']
+    install_requires=['cram', 'nose'],
+    long_description=_long_description,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Topic :: Software Development :: Bug Tracking']
 )
